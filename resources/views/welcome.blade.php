7abdffcfd12051ebab89b3996448b826ded8beba
victor chao <foureyeschao@gmail.com>
2022-10-28 22:33:17 -0400

    modification de description de App

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,7 +5,7 @@
             <div class="col-12 text-center pt-5">
                 <h1 class="display-one mt-5">{{ config('app.name') }}</h1>
                 <p>
-                    Ce blog génial contient de nombreux articles, cliquez sur le bouton ci-dessous</p> <br>
+                    Ce App génial contient de la liste des étudiants, cliquez sur le bouton ci-dessous</p> <br>
                 <a href="{{ route('index') }}" class="btn btn-outline-danger">La liste de l'étudiants</a>
             </div>
         </div>
